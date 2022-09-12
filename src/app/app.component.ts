3dc7f2a7c94ca13e26afd27796c7aa4b4db3eb0b
Gabriel Cavazani <gabriel_cavazani@hotmail.com>
2022-09-12 19:45:40 -0400

    Incluido APP Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({ 
+  selector: 'app-root', // Nome que vai invocar o component
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent { //
+  title = 'MinhaAppAngular';
+}
---
+++ src/app/funcionalidade/funcionalidade.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common'; //Quando tiver CommonModule ele é um modulo comum e pode ser utilizado em outros modulos que podem existir
+
+
+
+@NgModule({
+  declarations: [],
+      // Seus componentes
+
+
+  imports: [
+    CommonModule
+    // Seus Modulos
+  ],
+
+ providers: [
+  //Seus Servicos
+
+ ]
+
+})
+export class FuncionalidadeModule { }
