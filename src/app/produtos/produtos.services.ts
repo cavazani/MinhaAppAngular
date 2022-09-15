2e06678ab08795f4d049c4e3ae0484ae77d04b5f
Gabriel Cavazani <gabriel_cavazani@hotmail.com>
2022-09-14 21:29:08 -0400

    Criado arquivos Produtos

--- src/app/produtos/produtos.services.ts
+++ src/app/produtos/produtos.services.ts
@@ -1,8 +1,21 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http"
+import { Produto } from "./produto";
+import { Observable } from "rxjs";
 
-
+//Classe tipo de service que voce pode injetar em algum lugar
 @Injectable()
 export class ProdutosService{
 
-    protected UrlServiceV1: string = "http://localhost:3000/"
+
+constructor(private http: HttpClient){
+
+
+}
+
+    protected UrlServiceV1: string = "http://localhost:3000/";
+
+    obterProdutos() : Observable<Produto[]> {
+       return this.http.get<Produto[]>(this.UrlServiceV1 + "produtos")
+    }
 }
